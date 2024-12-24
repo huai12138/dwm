a6ea54fd0e5abcfa9f7e81dca800175d17c7dfd6
huai12138 <huaihuaidehuaihuai@gmail.com>
2024-12-24 14:38:21 +0800

    工作区指示替换为图标

--- config.def.h
+++ config.def.h
@@ -27,7 +27,7 @@ static const unsigned int alphas[][3]      = {
         [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "󰇄", "", "󰝚", "", "", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
