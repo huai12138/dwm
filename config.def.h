5a1a4ed718d493773ae15fb309ae179a1e546cff
huai12138 <huaihuaidehuaihuai@gmail.com>
2025-02-05 15:57:47 +0800

    add firefox

--- config.def.h
+++ config.def.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+        { "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
         { "Google-chrome",  NULL,       NULL,       1 << 0,       0,           -1 },
         { "Chromium",  NULL,       NULL,       1 << 0,       0,           -1 },
         { "TelegramDesktop",  NULL,       NULL,       1 << 1,       0,           -1 },
